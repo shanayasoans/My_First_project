5568bb3871dbd0caf786b901ed62af546ce28360
shanaya <shanayasoans31@gmail.com>
2021-01-23 07:50:37 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace My_First_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
